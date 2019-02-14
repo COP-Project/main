fef57463e936f50a81030a0a03ba7a47c873d608
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-02-13 20:40:43 -0500

    reads text
    but only from quality pictures

--- camCapt.py
+++ camCapt.py
@@ -1,13 +1,24 @@
 import cv2
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
 
 
 def main():
-  cam = cv2.VideoCapture(0)
+    #takes picture and saves it
+    #cam = cv2.VideoCapture(0)
+    #frame = cam.read()[1]
+    #cv2.imwrite(filename='img0.jpg', img=frame)
   
-  frame = cam.read()[1]
-  
-  cv2.imwrite(filename='img2.jpg', img=frame)
+    #takes text from specified image
+    im = Image.open('text.png')
+    strimg = pytesseract.image_to_string(im)
+    print(strimg)
   
 
 if __name__ == '__main__':
-  main()
+    main()
