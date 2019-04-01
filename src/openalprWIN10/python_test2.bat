3026122715d356cfd406685ccadf7698e3873424
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-04-01 19:14:33 -0400

    Rearranging some files

--- src/openalprWIN10/python_test2.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-python python/test.py -c us --config "openalpr.conf" --runtime_data "runtime_data" python/ferrari.jpg
-
-pause
--- src/testopenalpr.py
+++ src/testopenalpr.py
@@ -1,9 +1,9 @@
 from readPlate import readaPlate, create_alpr, destroy_alpr
 from openalpr import Alpr
 
-myalpr = create_alpr('us')
-x = readaPlate(myalpr, 'ca', 'ferrari.jpg')
+myalpr = create_alpr('sg')
+x = readaPlate(myalpr, 'sg', 'img/SINGAsg.jpg')
 destroy_alpr(myalpr)
-print(len(x))
+print(str(len(x)) + ' plate(s) found')
 for plate in x:
     print('Plate: ' + plate)
