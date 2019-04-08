e19638abaff7a79eceaf19b4ad9e39440aefa10c
AlexHillegass <alexhillegass@gmail.com>
2019-04-07 23:17:54 -0400

    functionality for reading plate from video stream

--- src/app/readPlate.py
+++ src/app/readPlate.py
@@ -20,3 +20,21 @@ def read_a_plate(img, region):
             list[i] = results['results'][i]['plate']
             i += 1
     return list
+
+
+def read_plate_from_stream(img, region):
+    platevalue = None
+    if not alpr.is_loaded():
+        print("Error loading OpenALPR")
+    else:
+        alpr.set_top_n(7)
+        alpr.set_default_region(region)
+        alpr.set_detect_region(False)
+        results = alpr.recognize_array(img)
+        i = 0
+        num_plates = len(results['results'])
+        list = [0]*num_plates            # how many plates were found
+        while i < num_plates:
+            list[i] = results['results'][i]['plate']
+            i += 1
+    return list
