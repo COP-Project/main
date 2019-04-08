f60e73f2a6325c735a97d86597ee5b3f8ca84948
Jesse Jamieson Abando <jjabando@knights.ucf.edu>
2019-04-07 22:27:35 -0400

    testopenalpr now has assertions

--- src/app/testopenalpr.py
+++ src/app/testopenalpr.py
@@ -1,13 +1,11 @@
 from readPlate import readaPlate, create_alpr, destroy_alpr
-from openalpr import Alpr
-    
-myalpr = create_alpr()
-x = readaPlate(myalpr, 'ca', '../img/ca.jpeg')
-print(str(len(x)) + ' plate(s) found')
-for plate in x:
-    print('Plate: ' + plate)
 
-x = readaPlate(myalpr, 'mt', '../img/mt.jpg')
-print(str(len(x)) + ' plate(s) found')
-for plate in x:
-    print('Plate: ' + plate)
+myalpr = create_alpr()
+
+def test_OALPR(myalpr, state, img):
+    plates = readaPlate(myalpr, state, img)
+    return plates[0]
+
+
+assert readaPlate(myalpr, 'mt', '../img/mt.jpg')[0] == 'BJR216'
+assert readaPlate(myalpr, 'ca', '../img/ca.jpeg')[0] == '7VDV740'
